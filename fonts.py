6047f2ff08da1bca3f1bcc5ca6421e821b7e3c6f
bbrianb <bbannon25@regis.org>
2024-07-21 20:38:29 -0400

    fixed fonts.py

--- fonts.py
+++ fonts.py
@@ -3,26 +3,30 @@ from tkinter import font
 
 root = Tk()
 root.title('Font Families')
-fonts=list(font.families())
+fonts = list(font.families())
 fonts.sort()
 
-def populate(frame):
-    '''Put in the fonts'''
-    listnumber = 1
+
+# noinspection PyUnresolvedReferences,PyShadowingNames,PyUnboundLocalVariable
+def populate(f):
+    """Put in the fonts"""
+    list_number = 1
     for i, item in enumerate(fonts):
-        label = "listlabel" + str(listnumber)
-        label = Label(frame,text=item,font=(item, 16))
+        label = Label(f, text=item, font=(item, 16))
         label.grid(row=i)
-        label.bind("<Button-1>",lambda e,item=item:copy_to_clipboard(item))
-        listnumber += 1
+        label.bind("<Button-1>", lambda e, item=item: copy_to_clipboard(item))
+        list_number += 1
+
 
 def copy_to_clipboard(item):
     root.clipboard_clear()
     root.clipboard_append("font=('" + item.lstrip('@') + "', 12)")
 
-def onFrameConfigure(canvas):
-    '''Reset the scroll region to encompass the inner frame'''
-    canvas.configure(scrollregion=canvas.bbox("all"))
+
+def on_frame_configure(c):
+    """Reset the scroll region to encompass the inner frame"""
+    c.configure(scrollregion=c.bbox("all"))
+
 
 canvas = Canvas(root, borderwidth=0, background="#ffffff")
 frame = Frame(canvas, background="#ffffff")
@@ -31,9 +35,10 @@ canvas.configure(yscrollcommand=vsb.set)
 
 vsb.pack(side="right", fill="y")
 canvas.pack(side="left", fill="both", expand=True)
-canvas.create_window((4,4), window=frame, anchor="nw")
+canvas.create_window((4, 4), window=frame, anchor="nw")
 
-frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas))
+# noinspection PyShadowingNames,PyUnresolvedReferences,PyUnboundLocalVariable
+frame.bind("<Configure>", lambda event, canvas=canvas: on_frame_configure(canvas))
 
 populate(frame)
 
